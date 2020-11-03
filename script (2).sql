ce6d6fc2e10d22b537f1baa2ce18ebbd34472fad
Aliaksandr Zelianko <zely20@mail.ru>
2020-11-03 13:53:35 +0300

    add script2

---
+++ script (2).sql
@@ -0,0 +1,16 @@
+select * from students;
+select name, course, speciality from students;
+select * from students where course = 2;
+select * from students where name is null;
+select * from students where name is not null;
+select * from students where enroll_date > '01.08.2020';
+select * from students where name like 'D%'; 
+select * from students where name like 'D%' and course > 2;
+select * from students where name like 'D%' or course > 2; 
+select current_date;
+select current_date > '10.09.2020'; 
+select current_date + interval '1 month';
+select * from students order by speciality asc;
+select * from students order by speciality desc;
+select distinct course from students; 
+select * from students limit 5; 
