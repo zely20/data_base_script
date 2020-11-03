df9a49e32e512499a16f4d2331742c29eeda8018
Aliaksandr Zelianko <zely20@mail.ru>
2020-11-03 15:17:00 +0300

    add join.sql

---
+++ join.sql
@@ -0,0 +1,41 @@
+
+create table departments(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table emploees(
+    id serial primary key,
+    name varchar(255),
+    departments_id int references departments(id)
+);
+
+insert into departments(name) values ('departments 1');
+insert into departments(name) values ('departments 2');
+insert into departments(name) values ('departments 3');
+
+insert into emploees(name, departments_id) values ('emploees 1', 1);
+insert into emploees(name, departments_id) values ('emploees 2', 1);
+insert into emploees(name, departments_id) values ('emploees 3', 2);
+insert into emploees(name, departments_id) values ('emploees 4', null);
+insert into emploees(name, departments_id) values ('emploees 5', 3);
+insert into emploees(name, departments_id) values ('emploees 6', 1);
+
+select * from departments d left join emploees e on e.departments_id = d.id;
+select * from departments d right join emploees e on e.departments_id = d.id;
+select * from departments d cross join emploees e;
+select * from departments d full join emploees e on e.departments_id = d.id;
+select * from emploees e left join departments d on e.departments_id = d.id where e.departments_id is null;
+
+create table teens(
+    name text primary key,
+    gender text
+);
+
+insert into teens(name, gender) values ('Alex','M');
+insert into teens(name, gender) values ('Olga','W');
+insert into teens(name, gender) values ('Pety','M');
+insert into teens(name, gender) values ('Vasya','M');
+
+select t1.gender as a, t2.gender as b from teens t1 cross join teens t2;
+//как сделать однополые браки не совсем понимаю
